9bda0c8467d1465195ccf86c059f599efb1ef6e1
madu-saints <madusantos1008@gmail.com>
2026-03-30 20:07:39 -0300

    Update Tosa.java

--- src/model/Tosa.java
+++ src/model/Tosa.java
@@ -1,4 +1,25 @@
 package model;
 
-public class Tosa {
+import enums.TamanhoAnimal;
+import java.time.LocalDate;
+
+public class Tosa extends Servico {
+
+    public Tosa(int codigo, LocalDate data, TamanhoAnimal tamanho) {
+        super(codigo, data, tamanho);
+    }
+
+    @Override
+    public double calcularPreco() {
+        if (getTamanho() == TamanhoAnimal.PEQUENO) this.preco = 30.0;
+        else if (getTamanho() == TamanhoAnimal.MEDIO) this.preco = 40.0;
+        else if (getTamanho() == TamanhoAnimal.GRANDE) this.preco = 50.0;
+
+        return this.preco;
+    }
+
+    @Override
+    public String getDescricao() {
+        return "Tosa (" + getTamanho() + ")";
+    }
 }
