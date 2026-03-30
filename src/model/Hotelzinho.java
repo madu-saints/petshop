f20e444e45985ba5ca9d6035a58ad5f9a5de9864
madu-saints <madusantos1008@gmail.com>
2026-03-30 20:05:43 -0300

    Update Hotelzinho.java

--- src/model/Hotelzinho.java
+++ src/model/Hotelzinho.java
@@ -1,4 +1,29 @@
 package model;
 
-public class Hotelzinho {
+import enums.TamanhoAnimal;
+import java.time.LocalDate;
+
+public class Hotelzinho extends Servico {
+    private int horas;
+
+    public Hotelzinho(int codigo, LocalDate data, TamanhoAnimal tamanho, int horas) {
+        super(codigo, data, tamanho);
+        this.horas = horas;
+    }
+
+    @Override
+    public double calcularPreco() {
+        double valorHora = 0;
+        if (getTamanho() == TamanhoAnimal.PEQUENO) valorHora = 12.0;
+        else if (getTamanho() == TamanhoAnimal.MEDIO) valorHora = 18.0;
+        else if (getTamanho() == TamanhoAnimal.GRANDE) valorHora = 25.0;
+
+        this.preco = valorHora * horas;
+        return this.preco;
+    }
+
+    @Override
+    public String getDescricao() {
+        return "Hotelzinho (" + getTamanho() + " - " + horas + " horas)";
+    }
 }
